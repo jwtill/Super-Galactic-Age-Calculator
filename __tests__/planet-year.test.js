bf0a09c190eeed293d4d7781e4673d7d7c51324f
Josh Tillinghast <jwtill@icloud.com>
2022-02-04 10:43:29 -0800

    Create test checking getMercuryYear

--- __tests__/planet-year.test.js
+++ __tests__/planet-year.test.js
@@ -3,7 +3,14 @@ import PlanetYear from './../src/planet-year.js';
 describe('PlanetYear', () => {
 
   test('should construct PlanetYear object', () => {
-    const planetYear = new PlanetYear("47");
-    expect(planetYear.age).toEqual("47");
+    const planetYear = new PlanetYear(47);
+    expect(planetYear.age).toEqual(47);
   });
 });
+
+describe('getMercuryYear', () => {
+
+  test('Should return persons age in Mercurian years', () => {
+    const planetYear = new PlanetYear(47);
+    expect(planetYear.getMercuryYear(47)).toEqual(11.28);
+  });
