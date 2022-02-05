e179194fc277c1e48207ec8b6366930e9fcc0331
Josh Tillinghast <jwtill@icloud.com>
2022-02-05 14:04:49 -0800

    Change tests back to reflect rounding only to two places

--- __tests__/planet-year.test.js
+++ __tests__/planet-year.test.js
@@ -18,7 +18,7 @@ describe('getMercuryAge', () => {
 
   test('Should return persons age in Mercurian years', () => {
     const planetYear = new PlanetYear(47);
-    expect(planetYear.getMercuryAge(47)).toEqual(11);
+    expect(planetYear.getMercuryAge(47)).toEqual(11.28);
   });
 });
 
@@ -26,7 +26,7 @@ describe('getVenusAge', () => {
 
   test('Should return persons age in Venusian years', () => {
     const planetYear = new PlanetYear(47);
-    expect(planetYear.getVenusAge(47)).toEqual(29);
+    expect(planetYear.getVenusAge(47)).toEqual(29.14);
   });
 });
 
@@ -34,7 +34,7 @@ describe('getVenusAge', () => {
 
   test('Should return persons age in Martian years', () => {
     const planetYear = new PlanetYear(47);
-    expect(planetYear.getMarsAge(47)).toEqual(88);
+    expect(planetYear.getMarsAge(47)).toEqual(88.36);
   });
 });
 
@@ -42,7 +42,7 @@ describe('getJupiterAge', () => {
 
   test('Should return persons age in Jovian years', () => {
     const planetYear = new PlanetYear(47);
-    expect(planetYear.getJupiterAge(47)).toEqual(557);
+    expect(planetYear.getJupiterAge(47)).toEqual(557.42);
   });
 });
 
