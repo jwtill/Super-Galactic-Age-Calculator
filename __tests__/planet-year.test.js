86a30ffe8989c01e36ba4194d74d25da698e79e1
Josh Tillinghast <jwtill@icloud.com>
2022-02-05 20:40:25 -0800

    Change planet age tests from mult to divis to get correct years on other planets.

--- __tests__/planet-year.test.js
+++ __tests__/planet-year.test.js
@@ -18,7 +18,7 @@ describe('getMercuryAge', () => {
 
   test('Should return persons age in Mercurian years', () => {
     const planetYear = new PlanetYear(47);
-    expect(planetYear.getMercuryAge(47)).toEqual(11.28);
+    expect(planetYear.getMercuryAge(47)).toEqual(195.83);
   });
 });
 
@@ -26,15 +26,15 @@ describe('getVenusAge', () => {
 
   test('Should return persons age in Venusian years', () => {
     const planetYear = new PlanetYear(47);
-    expect(planetYear.getVenusAge(47)).toEqual(29.14);
+    expect(planetYear.getVenusAge(47)).toEqual(75.81);
   });
 });
 
-describe('getVenusAge', () => {
+describe('getMarsAge', () => {
 
   test('Should return persons age in Martian years', () => {
     const planetYear = new PlanetYear(47);
-    expect(planetYear.getMarsAge(47)).toEqual(88.36);
+    expect(planetYear.getMarsAge(47)).toEqual(25);
   });
 });
 
@@ -42,7 +42,7 @@ describe('getJupiterAge', () => {
 
   test('Should return persons age in Jovian years', () => {
     const planetYear = new PlanetYear(47);
-    expect(planetYear.getJupiterAge(47)).toEqual(557.42);
+    expect(planetYear.getJupiterAge(47)).toEqual(3.96);
   });
 });
 
